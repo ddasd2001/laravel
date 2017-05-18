aaee227d067a97801b90f160b03ebfbe10077f8b
ddasd2001 <ddasd2001@qq.com>
2017-05-18 13:13:15 +0800

    喀什“

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Recorder;
@@ -13,9 +13,9 @@ class TestController extends Controller
 
     public function index()
     {
-        $result = Recorder::all();
-//        echo json_encode($result);
-        return view('Test.index', ['result' => $result]);
+        $result = Recorder::where('gameid', 118)->orderBy('id', 'desc')->Paginate(3);
+        return view('Test.index', compact('result'));
+//        print_r($result);
     }
 
 }
--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.default')
 @section('title', '战绩表')
 @section('content')
+    <div class="form-group">
     <table class="table table-striped">
         <thead>
         <tr>
-            <td rowspan=2>游戏id</td>
-            <td rowspan=2>用户id</td>
-            <td rowspan=3>局数</td>
-            <td rowspan=2>积分</td>
-            <td rowspan=2>游戏时间</td>
-            <td rowspan=2>特殊局标记</td>
+            <th>游戏id</th>
+            <th>用户id</th>
+            <th>局数</th>
+            <th>积分</th>
+            <th>游戏时间</th>
+            <th>特殊局标记</th>
         </tr>
         </thead>
         <tbody>
         @if (count($result) > 0)
             @foreach ($result  as  $a)
                 <TR>
-                    <TD>{{ $a->id }}</TD>
                     <TD>{{ $a->gameid }}</TD>
                     <TD>{{ $a->userid }}</TD>
                     <TD>{{ $a->code }}</TD>
@@ -27,5 +27,12 @@
             @endforeach
         @endif
         </tbody>
+        <tfoot>
+        <tr>
+            <td >111</td>
+        </tr>
+        </tfoot>
     </TABLE>
+    </div>
+    {{ $result->links() }}
 @stop
